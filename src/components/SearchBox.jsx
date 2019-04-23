85227b42f8f39fadaac501a4aa8d35f39c25c8f3
“Elizabeth <lizzyadeline@gmail.com>
2019-04-22 22:34:19 -0500

    Display nearby restaurants

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,29 +3,63 @@ import axios from 'axios';
 
 
 class SearchBox extends Component {
-  render() {
-    this.getPlaces();
-    return (
-      <div className="search-box"> 
-          <h1>Enter a zip code and explore dining options near your area.</h1>
-          <input type="text" placeholder="Search for restaurants near you" name="search"/>
-          <button type="submit"><i className="fa fa-search"></i></button>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = { 
+      restaurants: [],
+      zip_code: ""
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   getPlaces() {
     var params = {
-      zip_code: "60654"
+      zip_code: this.state.zip_code
     };
 
     axios.get('https://pacific-gorge-23307.herokuapp.com/places', {params})
     .then(response => {
-      console.log(response.data);
+      let restaurants = response.data.results;
+      this.setState({restaurants: restaurants});
+      console.log("state", this.state.restaurants);
     })  
     .catch(function(error) {
       console.log('GET REQUEST ERROR: \n', error);
     });
+  }
+
+  handleClick() {
+    console.log('API call');
+    this.getPlaces();
+  }
+
+  handleChange(event) {
+    this.setState({zip_code: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="Search-Box">
+        <div className="search-box"> 
+          <h1>Enter a zip code and explore dining options near your area.</h1>
+          <input value={this.state.zip_code} onChange={this.handleChange}type="text" placeholder="Search for restaurants near you" name="search"/>
+          <button onClick={this.handleClick} type="submit"><i className="fa fa-search"></i></button>
+        </div>
+
+          <div className="places">
+            {
+              this.state.restaurants.map((restaurant)=>{
+                return(
+                  <div key={restaurant.name}>
+                    <h4>{restaurant.name}</h4>
+                  </div>
+                )
+              })
+            }
+          </div>
+      </div>
+    );
   }
 
 }
