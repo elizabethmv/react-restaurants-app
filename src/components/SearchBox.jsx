a782bbdd9de1bad3df6f495d298e5957c1ac0db1
“Elizabeth <lizzyadeline@gmail.com>
2019-05-01 21:14:07 -0500

    Added checkbox feature (Vanilla JS) working correctly in console

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-
 class SearchBox extends Component {
   constructor(props) {
     super(props);
@@ -9,8 +8,34 @@ class SearchBox extends Component {
       restaurants: [],
       zip_code: ""
     };
+
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this._handleKeyDown = this._handleKeyDown.bind(this);
+  }
+
+  componentDidUpdate() {
+    var inputs = JSON.parse(localStorage.getItem('inputs'));
+    console.log(inputs);
+
+    inputs.forEach(function(input){
+      console.log(input);
+      if (document.getElementById(input.id) != null) {
+        document.getElementById(input.id).checked = input.checked;
+      }
+    });
+  }
+
+  getSavedPlaces(){
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var checkData = [];
+
+    checkboxes.forEach(function(input){
+      checkData.push({ id: input.id, checked: input.checked });
+    });
+
+    localStorage.setItem('inputs', JSON.stringify(checkData));
+    console.log(JSON.stringify(checkData));
   }
 
   getPlaces() {
@@ -30,8 +55,15 @@ class SearchBox extends Component {
   }
 
   handleClick() {
-    console.log('API call');
+    console.log('API call on CLICK');
     this.getPlaces();
+  }
+
+  _handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      console.log('API call on ENTER');
+      this.handleClick();
+    }
   }
 
   handleChange(event) {
@@ -42,8 +74,11 @@ class SearchBox extends Component {
     return (
       <div className="Search-Box">
         <div className="search-box"> 
-          <h1>Enter a zip code and explore dining options near your area.</h1>
-          <input value={this.state.zip_code} onChange={this.handleChange}type="text" placeholder="Search for restaurants near you" name="search"/>
+          {/* This will work with a componentDidMount() */}
+          {/* <input onChange={this.getSavedPlaces} type="checkbox" id="sample-check"></input> */}
+
+          <h1 id="test-id">Enter a zip code and explore dining options near your area.</h1>
+          <input value={this.state.zip_code} onChange={this.handleChange} onKeyDown={this._handleKeyDown} type="text" placeholder="Search for restaurants near you" name="search"/>
           <button onClick={this.handleClick} type="submit"><i className="fa fa-search"></i></button>
         </div>
 
@@ -52,7 +87,8 @@ class SearchBox extends Component {
               this.state.restaurants.map((restaurant)=>{
                 return(
                   <div key={restaurant.name}>
-                    <h4>{restaurant.name}</h4>
+                    <span><input id="test-check" onChange={this.getSavedPlaces} value={restaurant.name} type="checkbox"></input></span>
+                    <label>{restaurant.name}</label>
                   </div>
                 )
               })
